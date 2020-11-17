37a4676e0e07c6a7bc5acc328fbd20e88df09966
ralves <alves.rodrigo31@outlook.com>
2020-11-17 01:26:43 -0300

    Montagem do layout

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ const Home = () => {
   return (
     <div>
       <Header />
+
     </div>
   )
 }
@@ -11,7 +12,6 @@ const Home = () => {
 const Header = () => {
   return (
     <nav>
-      
       <ul>
         <li>Home</li>
         <li>Sobre</li>
@@ -19,6 +19,15 @@ const Header = () => {
       </ul>
     </nav>
   )
+}
+
+const Main = () => {
+  return (
+    <div>
+      <h1>Pagina Principal</h1>
+    </div>
+  )
+
 }
 
 export default Home;
